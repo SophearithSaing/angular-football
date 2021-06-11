981b72d2774f9aa04f1b5ebe86010ba351b5cf89
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-06-11 23:14:41 +0700

    update get fixture

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../environments/environment';
 import { FixtureStat } from './models/fixtureStat.model';
+import { Fixture } from './models/fixture.model';
 
 @Injectable({
   providedIn: 'root'
@@ -21,16 +22,16 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getFixtures = (league: string, date: string) => {
+  getFixtures = (league: number, date: string) => {
     const url = this.apiUrl + '/fixtures';
     const season = '2020';
-    return this.http.get<{response: Array<any>}>(url, {
+    return this.http.get<{response: Array<Fixture>}>(url, {
       headers: {
         'x-rapidapi-host': this.apiHost,
         'x-rapidapi-key': this.apiKey
       },
       params: {
-        league,
+        league: league.toString(),
         season,
         date
       }
